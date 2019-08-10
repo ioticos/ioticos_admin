af61af29555393f6af2d6aabee77d83418274185
root <root@ip-172-31-14-236.sa-east-1.compute.internal>
2019-08-10 21:28:47 +0000

    iniciamos conexion mqtt

--- app/application/views/scripts.php
+++ app/application/views/scripts.php
@@ -20,9 +20,10 @@
   src="https://code.jquery.com/jquery-3.4.1.min.js"
   integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
   crossorigin="anonymous"></script>
+
+  <script src="https://unpkg.com/mqtt/dist/mqtt.min.js"></script>
+
 <script type="text/javascript">
-
-
 
 window.onload = function() {
   <?php if($_SESSION['msg_body']!=""){ ?>
@@ -40,6 +41,50 @@ window.onload = function() {
     <?php } ?>
 };
 
+</script>
+
+<script type="text/javascript">
+const options = {
+  connectTimeout: 1000,
+  // Authentication
+  clientId: 'client_id_'+ Math.floor((Math.random() * 1000000) + 1),
+  username: 'VwaaCMWpQ1uTFJu',
+  password: '5F9z2Y46j3SeV5Q',
+  keepalive: 60,
+  clean: true,
+}
+
+// WebSocket connect url
+const WebSocket_URL = 'wss://ioticos.org:8094/mqtt';
+const client = mqtt.connect(WebSocket_URL, options)
+
+
+client.on('connect', () => {
+  console.log('Connect success');
+  client.subscribe('+/#', { qos: 0 }, (error) => {
+    if(error){
+
+    }else{
+
+    }
+
+  })
+})
+
+client.on('message', (topic, message) => {
+  console.log('Mensaje recibido bajo tópico: ', topic, ' -> ', message.toString());
+$("#display_output").append("Tópico ⇨ "+topic+" Mensaje ⇨ "+message.toString()+"<br>");
+})
+
+client.on('reconnect', (error) => {
+  console.log('reconnecting:', error);
+  $("#display_status").append("🔵 Reconectando... :(<br>");
+})
+
+client.on('error', (error) => {
+  console.log('Connect Error:', error);
+  $("#display_status").append("🔴 Error en la conexión :(<br>");
+})
 
 
 
