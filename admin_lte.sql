54f8f4e4caa4502bcd7471c49da45b835a857f33
root <root@ip-172-31-13-67.sa-east-1.compute.internal>
2019-09-05 22:12:26 +0000

    corrección en archivo .sql para que importe bien desde phpmyadmin

--- admin_lte.sql
+++ admin_lte.sql
@@ -1,29 +1,6 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 4541
-#
-# http://www.sequelpro.com/
-# https://github.com/sequelpro/sequelpro
-#
-# Host: ioticosadmin.ml (MySQL 5.7.27-0ubuntu0.18.04.1)
-# Database: admin_lte
-# Generation Time: 2019-08-29 20:18:58 +0000
-# ************************************************************
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 
 # Dump of table data
 # ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `data`;
 
 CREATE TABLE `data` (
   `data_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -32,14 +9,12 @@ CREATE TABLE `data` (
   `data_hum` int(3) NOT NULL,
   `data_device_sn` varchar(10) NOT NULL,
   PRIMARY KEY (`data_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=92 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 
 # Dump of table devices
 # ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `devices`;
 
 CREATE TABLE `devices` (
   `device_id` int(7) unsigned NOT NULL AUTO_INCREMENT,
@@ -49,7 +24,8 @@ CREATE TABLE `devices` (
   `device_sn` int(7) DEFAULT NULL,
   `device_topic` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`device_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 
 
@@ -57,8 +33,6 @@ CREATE TABLE `devices` (
 
 # Dump of table users
 # ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE `users` (
   `user_id` int(7) NOT NULL AUTO_INCREMENT,
@@ -70,14 +44,12 @@ CREATE TABLE `users` (
   `user_image` varchar(200) NOT NULL DEFAULT 'https://cdn1.iconfinder.com/data/icons/avatars-vol-2/140/_robocop-512.png',
   `user_selected_device` int(7) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 
 
 
-# Replace placeholder table for devices_full with correct view syntax
-# ------------------------------------------------------------
 
 
 CREATE OR REPLACE VIEW `devices_full`
@@ -89,10 +61,3 @@ AS SELECT
    `data`.`data_device_sn` AS `data_device_sn`,
    `devices`.`device_id` AS `device_id`
 FROM (`data` join `devices`) where (`devices`.`device_sn` = `data`.`data_device_sn`);
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
